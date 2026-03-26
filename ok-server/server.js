7bf0e6410c82a4a626ab7f0c0f4099fd6b09ff79
Sourabh <me.sourabhrustagi@gmail.com>
2026-03-26 08:11:16 +0530

    Add /echo route

--- ok-server/server.js
+++ ok-server/server.js
@@ -3,6 +3,17 @@ import http from 'http';
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
+  const url = new URL(req.url, `http://${req.headers.host || 'localhost'}`);
+
+  // GET /echo?msg=hello -> "hello"
+  if (url.pathname === '/echo') {
+    const msg = url.searchParams.get('msg') ?? '';
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+    res.end(msg);
+    return;
+  }
+
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain; charset=utf-8');
   res.end('ok');
